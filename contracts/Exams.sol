812eaddbdb157399b8aa14d2807bd822476ee8d8
criptoanarquist <alejoveintimilla@gmail.com>
2019-11-27 12:35:24 -0500

    addExam return statement

--- contracts/Exams.sol
+++ contracts/Exams.sol
@@ -8,7 +8,7 @@ contract Exams {
     mapping(address => string) professorsExam;
 
     struct Exam {
-        mapping(address => bool) examSuccess;
+        mapping(string => bool) examSuccess;
     }
 
     mapping(string => Exam) examHash;
@@ -23,11 +23,12 @@ contract Exams {
         _;
     }
 
+
     function addExam(string memory hash) public returns (string memory examProfessorHash) {
         // save the exam hash and link it with the professors address
         professorsExam[msg.sender] = hash;
 
-        return hash;
+        return professorsExam[msg.sender];  // TODO this works but returns 0
     }
 
     function isOwner(string memory hash) public view returns (bool) {
@@ -39,25 +40,12 @@ contract Exams {
         }
     }
 
-    function studentAddExam(address studentAddress) public returns (string memory hash){
+    function studentAddExam(string memory hash) public view returns (bool){
         // Receives a student's address and adds it to the people that passed that exam
         // Only the creator of the exam can do this
         string memory professorsExamHash = professorsExam[msg.sender];  // The hash of the exam owned by msg.sender
-        if(bytes(professorsExamHash).length == 0){
-            return "No exam hash asociated with this professor address";
-        }
-        Exam storage examObj = examHash[professorsExamHash];  // Get the correct Exam struct
-        examObj.examSuccess[studentAddress] = true;  // updates the examSuccess mapping with the student hash pointing to true
-        return (professorsExamHash);
-    }
-
-    function checkStudentExam(string memory examCheckHash) public view returns (string memory hash){
-        // Receives an Exam Hash and checks weather message.sender is recorded as having completed that exam
-        Exam storage examObj = examHash[examCheckHash];
-        if (examObj.examSuccess[msg.sender] == true){
-            return examCheckHash;
-        } else {
-            return "Student address not recorded as having passed the exam";
-        }
+        Exam storage examObj = examHash[professorsExamHash];
+        bool studentPassedExam = examObj.examSuccess[hash];
+        return (studentPassedExam);
     }
 }
